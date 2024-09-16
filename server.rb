83f04dd5ea95cf7384d59955ab9127f8129e82d8
Takahiro Masui <takahiro.masui@luxiar.com>
2024-09-16 10:07:53 +0900

    feat: Update server.rb to improve request handling

--- server.rb
+++ server.rb
@@ -4,43 +4,53 @@ require_relative 'http_request_parser'
 require_relative 'http_response_parser'
 
 SERVER_ROOT_PATH = File.expand_path('src', __dir__)
-server = TCPServer.new('localhost', 9292)
 
-def prepare(parsed_request)
-  path = parsed_request[:path]
-  if path == '/'
-    respond_with(SERVER_ROOT_PATH + '/index.html')
-  else
-    respond_with(SERVER_ROOT_PATH + path)
+class Kp::HttpServer
+  def initialize(port)
+    @port = port
+  end
+
+  def start
+    server = TCPServer.new('localhost', @port)
+
+    loop do
+      socket = server.accept
+      request = socket.readpartial(2048)
+      parsed_request = Kp::HttpRequestParser.new(request).parse
+    
+      STDERR.puts parsed_request
+    
+      response = prepare(parsed_request)
+      response.send(socket)
+    
+      socket.close
+    end
+  end
+
+  def prepare(parsed_request)
+    path = parsed_request[:path]
+    if path == '/'
+      respond_with(SERVER_ROOT_PATH + '/index.html')
+    else
+      respond_with(SERVER_ROOT_PATH + path)
+    end
+  end
+  
+  def respond_with(path)
+    if File.exist?(path)
+      ok_response(File.binread(path))
+    else
+      not_found_response
+    end
+  end
+  
+  def ok_response(body)
+    Kp::HttpResponseParser.new(code: 200, body:)
+  end
+  
+  def not_found_response
+    Kp::HttpResponseParser.new(code: 404)
   end
 end
 
-def respond_with(path)
-  if File.exist?(path)
-    ok_response(File.binread(path))
-  else
-    not_found_response
-  end
-end
-
-def ok_response(body)
-  Kp::HttpResponseParser.new(code: 200, body:)
-end
-
-def not_found_response
-  Kp::HttpResponseParser.new(code: 404)
-end
-
-loop do
-  socket = server.accept
-  request = socket.readpartial(2048)
-  parsed_request = Kp::HttpRequestParser.new(request).parse
-
-  STDERR.puts parsed_request
-
-  response = prepare(parsed_request)
-  response.send(socket)
-
-  socket.close
-end
-
+Kp::HttpServer.new(9292).start
